db918feae8588e1a7ff81d39348212de7772f038
maxmin93 <maxmin93@gmail.com>
2020-09-25 11:38:30 +0900

    add java-jdbc-test

---
+++ java-jdbc-test/src/test/java/net/bitnine/agens/hive/jdbctest/JdbcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bitnine.agens.hive.jdbctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
